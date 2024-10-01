7a339b8d786c57bab8aaeaabb54ad521cb246b92
bankashi <trinigun.bankashi@gmail.com>
2024-09-30 21:57:32 -0500

    Agregado variante postgresql para docker

--- demo_api/database/database.py
+++ demo_api/database/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine, MetaData, Table
 
 # Configuración de la conexión a la base de datos
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://conversiones_user:muses_c_us_27809@127.0.0.1:3306/prueba_api_conversiones")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://conversiones_user:muses_c_us_27809@172.40.1.20:5432/prueba_api_conversiones")
 
 # Crear el motor de la base de datos
 engine = create_engine(DATABASE_URL, echo=True)  # Cambiar a False en producción
